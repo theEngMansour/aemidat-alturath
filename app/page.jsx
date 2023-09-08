427b854b026418b7f53f7c5b7a38e1e7774de475
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2023-09-09 00:04:57 +0300

    the add hero amd steps

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing/Hero";
+import Steps from "@/components/landing/Steps";
 
 export default function page() {
   return (
     <>
       <Hero />
+      <Steps />
     </>
   );
 }
--- components/landing/Hero.jsx
+++ components/landing/Hero.jsx
@@ -5,68 +5,46 @@ import { TypeAnimation } from "react-type-animation";
 export default function Hero() {
   return (
     <div className="relative bg-gradient-to-b from-green-50 to-green-100">
-      <header className="absolute inset-x-0 top-0 z-10 w-full">
+      <header className="w-full pt-4">
         <div className="px-4 mx-auto sm:px-6 lg:px-8">
           <div className="flex items-center justify-end h-16 lg:h-20">
             <div className="flex-shrink-0">
-              <a href="#" title="" className="flex mt-8">
+              <span title="" className="flex mt-8">
                 <Image src="/logo.svg" width={250} height={50} alt="" />
-              </a>
+              </span>
             </div>
           </div>
         </div>
       </header>
 
-      <section className="overflow-hidden">
-        <div className="flex flex-col lg:flex-row lg:items-stretch lg:max-h-[900px] lg:min-h-[900px]">
-          <div className="flex items-center justify-center w-full lg:order-2 lg:w-7/12">
-            <div className="h-full px-4 pt-24 pb-16 sm:px-6 lg:px-24 2xl:px-32 lg:pt-40 lg:pb-14">
-              <div
-                dir="rtl"
-                className="flex flex-col justify-between flex-1 h-full text-center sm:text-right"
-              >
-                <div>
-                  <h1 className="text-5xl font-bold mt-8 text-black sm:text-6xl xl:text-7x text-rightl">
-                    <TypeAnimation
-                      sequence={[
-                        // Same substring at the start will only be typed once, initially
-                        "تركيب بيوت الشعر",
-                        1000,
-                        "خيام باكستانية",
-                        1000,
-                        "شرعات الماء",
-                        1000,
-                      ]}
-                      speed={50}
-                      className="font-black text-green-700"
-                      repeat={Infinity}
-                    />
-                  </h1>
-                  <p className="mt-6 text-base text-black sm:text-xl font-regular">
-                    اعمده التراث لبيوت الشعر و الخيام و المظلات تبوك
-                  </p>
-                  <a
-                    href="#"
-                    title=""
-                    className="inline-flex items-center px-6 py-5 text-base font-semibold text-black transition-all duration-200 bg-green-300 mt-9 hover:bg-green-400 focus:bg-green-400"
-                    role="button"
-                  >
-                    {" "}
-                    Get started for free{" "}
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="relative w-full overflow-hidden lg:w-5/12 lg:order-1">
-            <div className="lg:absolute lg:bottom-0 lg:left-0">
-              <img
-                className="w-full"
-                src="/phone-mockup.png"
-                alt=""
+      <section className="relative py-12 sm:py-16 lg:pt-20 xl:pb-0">
+        <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl pb-10 md:pb-32">
+          <div className="max-w-3xl mx-auto text-center">
+            <p className="inline-flex font-regular px-4 py-2 text-base text-green-700 border border-gray-200 rounded-full font-pj">
+              مرحباً بكم
+            </p>
+            <h1
+              dir="rtl"
+              className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj"
+            >
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed once, initially
+                  "تركيب بيوت الشعر",
+                  1000,
+                  "تركيب خيام باكستانية",
+                  1000,
+                  "تركيب شرعات الماء",
+                  1000,
+                ]}
+                speed={50}
+                className="font-black text-green-700"
+                repeat={Infinity}
               />
-            </div>
+            </h1>
+            <p className="max-w-md mx-auto mt-6 text-base leading-7 text-gray-600 font-regular">
+              اعمده التراث لبيوت الشعر و الخيام و المظلات تبوك
+            </p>
           </div>
         </div>
       </section>
